aee00b964ec258cf68e6ed90f829bb6b8a727bb4
atott <BNV36Fklm>
2013-01-19 14:22:41 +0400

    Initial commit

--- src/com/epam/diordna/helloWorld/MyActivity.java
+++ src/com/epam/diordna/helloWorld/MyActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-import org.w3c.dom.Text;
 
 public class MyActivity extends Activity {
     @Override
